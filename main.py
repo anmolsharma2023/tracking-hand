adfed50ceb70004ec4bdfeea38380bbecc960da4
anmol sharma <way2anmolsharma09@gmail.com>
2023-06-12 14:56:01 +0530

    do

--- main.py
+++ main.py
@@ -15,20 +15,20 @@ while True:
     hands, img = detector.findHands(img)
     img1 = cv2.imread(r"C:\Users\Haha CORPORATION\Desktop\zoom\building.jpg")
     if len(hands) == 2:
-        #print(detector.fingersUp(hands[0]), detector.fingersUp(hands[1]))
+       
         if detector.fingersUp(hands[0]) == [1, 1, 0, 0, 0] and \
                 detector.fingersUp(hands[1]) == [1, 1, 0, 0, 0]:
-            # print("Zoom Gesture")
+           
             lmList1 = hands[0]["lmList"]
             lmList2 = hands[1]["lmList"]
-            # point 8 is the tip of the index finger
+           
             if startDist is None:
-                #length, info, img = detector.findDistance(lmList1[8], lmList2[8], img)
+                
                 length, info, img = detector.findDistance(hands[0]["center"], hands[1]["center"], img)
 
                 startDist = length
 
-            #length, info, img = detector.findDistance(lmList1[8], lmList2[8], img)
+     
             length, info, img = detector.findDistance(hands[0]["center"], hands[1]["center"], img)
 
             scale = int((length - startDist) // 2)
